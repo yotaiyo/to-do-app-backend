cabdbb899f34719cd4a70c6342caa8ee69119d6e
yotaiyo <yota071218@gmail.com>
2019-08-05 20:00:35 +0900

    add origin

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Backend
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'http://localhost:3000'
+        origins 'http://localhost:3000', 'https://yotaiyo-to-do-app.firebaseapp.com'
         resource '*',
         :headers => :any,
         :methods => [:get, :post, :patch, :delete, :options]
